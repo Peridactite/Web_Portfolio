eff1594cbd69c21a6cb2cb0b70b5786f3587dc2f
dwoodman <dwoodman@uncc.edu>
2019-02-23 14:27:17 -0500

    About and Dev pages can be scrolled to with jquery. Working on button icons and styles.

--- portfolio/script.js
+++ portfolio/script.js
@@ -1,1 +1,17 @@
-javascript
+//This scrolls to the about div
+$(document).ready(function (){
+	$("#aboutLink").click(function (){
+		$('html, body').animate({
+			scrollTop: $("#aboutDiv").offset().top
+		}, 2000);
+	});
+});
+
+//This scrolls to the dev div
+$(document).ready(function (){
+	$("#devLink").click(function (){
+		$('html, body').animate({
+			scrollTop: $("#devDiv").offset().top
+		}, 2000);
+	});
+});
